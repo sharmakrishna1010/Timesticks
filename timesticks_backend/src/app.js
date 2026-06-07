f5ea6007859d7d2b274af8c026511c7f2a68741b
paudelkrishna1010 <paudelkrishna1010@gmail.com>
2026-06-07 19:26:49 +0530

    Update CORS origin and API base URL for production deployment

--- timesticks_backend/src/app.js
+++ timesticks_backend/src/app.js
@@ -9,7 +9,7 @@ import taskRoutes from "./routes/task.routes.js"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://timesticks.onrender.com',
     credentials: true,
 }));
 
