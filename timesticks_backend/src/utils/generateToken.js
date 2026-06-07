0d7c017338952646c90c22054634630093aea3fd
paudelkrishna1010 <paudelkrishna1010@gmail.com>
2026-06-07 19:48:13 +0530

    Update cookie setting

--- timesticks_backend/src/utils/generateToken.js
+++ timesticks_backend/src/utils/generateToken.js
@@ -6,11 +6,11 @@ const generateTokenAndSetCookie = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, 
+    maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, 
-    sameSite: "strict", 
-    secure: process.env.NODE_ENV !== "development",
-  });
+    sameSite: "none",
+    secure: true, 
+});
 };
 
 export default generateTokenAndSetCookie;
