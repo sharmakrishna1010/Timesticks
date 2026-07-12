a8cac3e060b0473ba0094c72fd3694b150284a8b
paudelkrishna1010 <paudelkrishna1010@gmail.com>
2026-07-12 12:37:43 +0530

    bug fix in loading

--- timesticks_frontend/src/main.tsx
+++ timesticks_frontend/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Remove the HTML splash screen the moment React takes over.
+// It was shown while the JS bundle was loading (before this line runs).
+const splash = document.getElementById('ts-splash');
+if (splash) splash.remove();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
